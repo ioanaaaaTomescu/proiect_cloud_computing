5e13753fd37ef5f58da6b1e52cd05009eda6ce7d
Ioana Tomescu <112384683+ioanaaaaTomescu@users.noreply.github.com>
2024-05-10 20:43:24 +0300

    Add files via upload

---
+++ WebForm1.aspx
@@ -0,0 +1,106 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="proiect_cloud_ioana_tomescu.WebForm1" Async="true" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Afla cum o sa fie vremea in orasul tau!</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+        }
+        .container {
+            max-width: 400px;
+            margin: 50px auto;
+            padding: 20px;
+            background-color: #fff;
+            border-radius: 5px;
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+        }
+        h1 {
+            text-align: center;
+            margin-bottom: 20px;
+        }
+        label {
+            display: block;
+            margin-bottom: 10px;
+        }
+        input[type="text"] {
+            width: 100%;
+            padding: 10px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            box-sizing: border-box;
+            margin-bottom: 20px;
+        }
+        button {
+            width: 100%;
+            padding: 10px;
+            background-color: #007bff;
+            color: #fff;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+        button:hover {
+            background-color: #0056b3;
+        }
+        .weather-data {
+            margin-top: 20px;
+            padding: 10px;
+            background-color: #f9f9f9;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+        }
+    .weather-data-label {
+        font-size: 18px;
+        font-weight: bold;
+        color: #333;
+        margin-bottom: 10px;
+        align-items: center;
+
+    }
+
+
+    .temperature-label {
+        font-size: 16px;
+        color: #666;
+    }
+
+    </style>
+</head>
+<body>
+     <form id="weatherForm" runat="server">
+        <div class="container">
+            <h1>Afla cum o sa fie vremea in orasul tau!</h1>
+             <asp:TextBox ID="txtCity" runat="server" CssClass="form-control" placeholder="Alege un oras dar scrie in engleza!" Required="true"></asp:TextBox>
+             <asp:Button ID="btnSubmit" runat="server" Text="Afla vremea" CssClass="btn btn-primary" OnClick="btnSubmit_Click" />
+            <br />
+            <div>
+              <asp:Label id="lblWeatherCity" runat="server" CssClass="weather-data-label" ></asp:Label>
+            </div> 
+            <div>
+                <asp:Label id="Label1" runat="server" CssClass="weather-data-label"></asp:Label>
+            </div> 
+            <div>
+                <asp:Label id="lblTemperature" runat="server" CssClass="temperature-label"></asp:Label>
+            </div>
+            <div>
+                <asp:Label id="lblTempMax" runat="server" CssClass="temperature-label"></asp:Label>
+            </div>
+            <div>
+                <asp:Label id="lblTempMin" runat="server" CssClass="temperature-label"></asp:Label>
+            </div>           
+            <div>
+                <asp:Label id="lblHumidity" runat="server" CssClass="temperature-label"></asp:Label>
+            </div>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.Windows.Forms;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+
+namespace proiect_cloud_ioana_tomescu
+{
+    public partial class WebForm1 : System.Web.UI.Page
+
+    {
+    
+        IFirebaseClient client;
+        protected void Page_Load(object sender, EventArgs e) 
+        {
+            IFirebaseConfig config = new FirebaseConfig
+            {
+                AuthSecret = "JanSAnPFCwRoSfP4FtyQhIuaibl3yj0ohId1zbxp",
+                BasePath = "https://proiect-cloud-c2a96-default-rtdb.europe-west1.firebasedatabase.app/"
+            };
+            client = new FireSharp.FirebaseClient(config);
+
+        }
+
+        private async Task SaveDataToFirebaseAsync()
+        {
+            var data = new Data
+            {
+                cityJson = lblWeatherCity.Text,
+                temperatureJson = lblTemperature.Text,
+                temperatueMin = lblTempMin.Text,
+                temperatureMax = lblTempMax.Text,
+                humidity = lblHumidity.Text
+            };
+
+            SetResponse response = await client.SetTaskAsync("Orase/" + data.cityJson, data);
+            Data result = response.ResultAs<Data>();
+        }
+
+        public async void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string city = txtCity.Text;
+
+            // Fetch weather data from OpenWeatherMap API
+            string apiKey = "f5cbb4cc5be65002ffecf98ab55dea1e"; // Replace with your API key
+            string apiUrl = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}";
+            if (city.Length == 0 || city == null)
+            {
+                lblWeatherCity.Text = "Te rog alege un oras!";
+            }
+            else
+            {
+                string weatherData = await GetWeatherData(apiUrl);
+                string cityJson = getCityName(weatherData);
+                double temperatureJson = getTemp(weatherData);
+                double temperatueMin = getTempMin(weatherData);
+                double temperatureMax = getTempMax(weatherData);
+                int humidity = GetHumidity(weatherData);
+                // Display weather data
+                lblWeatherCity.Text = "Oras: " + cityJson;
+                lblTemperature.Text = "Temperatura: " + temperatureJson.ToString();
+                lblTempMin.Text = "Temperatura minima: " + temperatueMin.ToString();
+                lblTempMax.Text = "Temperatura maxima: " + temperatureMax.ToString();
+                lblHumidity.Text = "Umiditate: " + humidity.ToString();
+                await SaveDataToFirebaseAsync();
+                MessageBox.Show("Data inserted");
+            }
+        }
+
+        private async Task<string> GetWeatherData(string apiUrl)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                return responseBody;
+            }
+        }
+        static double KelvinToCelsius(double kelvin)
+        {
+            return kelvin - 273.15;
+        }
+
+        static double getTemp(string JsonString)
+        {
+            dynamic weatherData = new JavaScriptSerializer().DeserializeObject(JsonString);
+
+            return KelvinToCelsius((double)weatherData["main"]["temp"]);
+
+        }
+        static double getTempMin(string JsonString)
+        {
+            dynamic weatherData = new JavaScriptSerializer().DeserializeObject(JsonString);
+
+            return KelvinToCelsius((double)weatherData["main"]["temp_min"]);
+
+        }
+
+        static double getTempMax(string JsonString)
+        {
+            dynamic weatherData = new JavaScriptSerializer().DeserializeObject(JsonString);
+
+            return KelvinToCelsius((double)weatherData["main"]["temp_max"]);
+
+        }
+
+        static int GetHumidity(string JsonString)
+        {
+            dynamic weatherData = new JavaScriptSerializer().DeserializeObject(JsonString);
+
+            return weatherData["main"]["humidity"];
+        }
+
+        static string getCityName(string JsonString)
+        {
+            dynamic weatherData = new JavaScriptSerializer().DeserializeObject(JsonString);
+
+            return weatherData["name"];
+        }
+
+        internal class Data
+        {
+            public string cityJson;
+            public string temperatureJson { get; set; }
+            public string temperatueMin { get; set; }
+            public string temperatureMax { get; set; }
+            public string humidity { get; set; }
+        }
+
+    }
+}
